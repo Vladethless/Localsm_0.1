99b6e8d01ae0e31ad0f523c7d23c2e54d5e996de
Dyshkant Vlad <vladethless@ukr.net>
2021-08-02 23:49:09 +0300

    + правки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('.slider_inner').slick({
+    $('.slider_inner, .clients_slider_inner').slick({
         nextArrow: '<button type = "button" class="slick-btn slick-next"></ button>',
         prevArrow: '<button type = "button" class="slick-btn slick-prev"></ button>',
         infinite: false,
